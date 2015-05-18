d9b30d5dfe4d89067e7e8dcc21064b0c0971083f
Minzl <m.ostermaier@googlemail.com>
2015-05-18 13:06:00 +0200

    endlich Feind
    terminal buggt. github ftw

--- Meilenstein3/check.js
+++ Meilenstein3/check.js
@@ -1,100 +1,86 @@
-
 function checkjava() {
 
-    var bool = false;
-    var fehler = true;
 
-    function exception() {
-        if (fehler == false) {
-            alert(unescape("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben"));
-        }
-        return fehler;
+    var nurBuchstaben = /^[\sa-zA-ZßäöüÄÖÜ]+$/; /*erstellt Variable die alle Buchstaben enthaellt*/
+    var nurZahlen =/^[0-9]+$/;                      /*erstellt Variable die alle Zahlen enthaelt*/
+    aktuellesDatum = new Date();                    /*speichert aktuelles Datum*/
+    aktuellesJahr = aktuellesDatum.getFullYear();   /*speichert aktuelles Jahr in der Variable*/
+
+    /*Überprüfung des Titelfeldes
+    Es wird geschaut ob das Feld freigelassen wurde. Wenn ja gibt es eine Fehlermeldung
+    und das feld wird rot umrandet
+    */
+    if(btitel.value==""){
+
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        document.Formular.btitel.focus();
+        document.Formular.titel.style.borderColor = "red";
+        return false;
+
+ 
+    }
+     /*Überprüfung des autorfeldes
+     Es wird geprüft ob nur Buchstaben eingegeben wurden
+     */
+    if (!nurBuchstaben.test(autor.value)) {
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        document.Formular.autor.focus();
+        document.Formular.autor.style.borderColor = "red";
+        return false;
+    }
+     /*Überprüfung des isbnfeldes
+     Es wird geprüft ob nur maximal 13 zahlen eingegeben wurden
+     */
+    if((!nurZahlen.test(isbn.value)) || (isbn.value.length >13)) {
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        document.Formular.isbn.focus();
+        document.Formular.isbn.style.borderColor = "red";
+        return false;
+    }
+     /*Überprüfung des vornamefeldes
+     Es wird geprüft ob nur Buchstaben eingegeben wurden
+     */
+    if (!nurBuchstaben.test(vorname.value)) {
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        document.Formular.vorname.focus();
+        document.Formular.vorname.style.borderColor = "red";
+        return false;
+    }
+     /*Überprüfung des nachnamefeldes
+     es wird geprüft ob nur Buchstaben eingegeben wurden
+     */
+    if (!nurBuchstaben.test(nachname.value)) {
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        document.Formular.nachname.focus();
+        document.Formular.nachname.style.borderColor = "red";
+        return false;
+    }
+     /*Überprüfung des erscheinungsjahrfeldes
+     es wird geprüft ob -die länge größer 4 ist
+                        -der wert über 1 ist
+                        -der wert nicht über das aktuelle Jahr schreitet.
+                        Hierzu wird die im Kopf bereits deklarierte variable aktuellesJahr
+                        verwendet
+    */
+    if(erscheinungsjahr.value.length >4|| erscheinungsjahr.value<1 || erscheinungsjahr.value>aktuellesJahr) {
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        document.Formular.erscheinungsjahr.focus();
+        document.Formular.jahr.style.borderColor = "red";
+
+        return false;
+    }
+     /*Überprüfung des auflagefeldes
+     Es wird geprüft -nur eingabe von zahlen
+                     -die eingabe größer 1 ist
+                     -und die eingabe genau aus einer zahl besteht
+    */
+    if (!nurZahlen.test(auflage.value)||(auflage.value<0)||(auflage.value.length!=1)) {
+        alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+        document.Formular.auflage.focus();
+        document.Formular.auflage.style.borderColor = "red";
+        return false;
     }
 
-    var btitel = document.getElementById("btitel").value;
-
-    if (btitel.length < 1) {
-        if (bool == false) {
-            document.getElementById("btitel").focus();
-            bool = true;
-        }
-        document.getElementById("btitel").style.borderColor = "red";
-        exception();
-    }
-
-    var autor = document.getElementById("autor").value;
-    if (!(autor.match(/[a-zA-Z]+$/))) {
-        if (bool == false) {
-            document.getElementById("autor").focus();
-            bool = true;
-        }
-        document.getElementById("autor").style.borderColor = "red";
-        exception();
-    }
-
-    var isbn = document.getElementById("isbn").value;
-
-    if (isbn.match([0-9]+$)) {
-
-        if (isbn.length != 13) {
-            if (bool == false) {
-                document.getElementById("isbn").focus();
-                bool = true;
-            }
-            document.getElementById("isbn").style.borderColor = "red";
-            exception();
-        }
-    }
-
-    var jahr = document.getElementById("jahr").value;
-
-    if (jahr.match([0-9]+$)) {
-
-        if (jahr.length != 4) {
-
-            document.getElementById("jahr").style.borderColor = "red";
-            exception();
-        }
-        else {
-        if (bool == false) {
-            document.getElementById("jahr").focus();
-            bool = true;
-        }
-        document.getElementById("jahr").style.borderColor = "red";
-            exception();
-    }
-
-
-    var auflage = document.getElementById("auflage").value;
-    if (auflage.match([0-9]+$)) {
-        if(bool = false){
-            document.getElementById("auflage").focus();
-            bool = true;
-        }
-    }
-        document.getElementById("auflage").style.borderColor = "red";
-        exception();
-    }
-
-
-    var vorname = document.send.vorname.value;
-    if (!(vorname.match(/[a-zA-Z]+$/))) {
-        if (bool == false) {
-            document.send.vorname.focus();
-            bool = true;
-        }
-        document.send.vorname.style.borderColor = "red";
-        exception();
-    }
-
-    var name = document.getElementById("nachname").value;
-    if (!(name.match(/[a-zA-Z]+$/))) {
-        if (bool == false) {
-            document.getElementById("vorname").focus();
-            bool = true;
-        }
-        document.getElementById("vorname").style.borderColor = "red";
-        exception();
-    }
 
 }
+
