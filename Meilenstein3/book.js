877c36d50049d7511eac9a3497c924c1a677f672
Philipp <scholz_philipp@gmx.de>
2015-05-18 04:53:29 +0200

    finish!

--- Meilenstein3/book.js
+++ Meilenstein3/book.js
@@ -1,35 +1,58 @@
+/* Erstellt Http request*/
 var json = new XMLHttpRequest();
 
+    /* bindet JSON file ein*/
     json.open("GET", "horror_books.json",false);
+
+    /* sendet request*/
     json.send();
+
+    /* variable horror beinhaltet horror tabelle*/
     var horror = JSON.parse(json.responseText);
     horror = horror.horrordata;
 
 
-
+    /*gleiches Spiel wie Horror*/
     json.open("GET", "roman_books.json",false);
     json.send();
     var roman = JSON.parse(json.responseText);
     roman = roman.romandata;
 
 
-
+/* Zeigt Horrortabelle an */
 function Horrorbutton(){
     document.getElementById('romantabelle').style.visibility="hidden";
     document.getElementById('horrortabelle').style.visibility="visible";
 
 }
 
-
+/* Zeigt Romantabelle an*/
 function Romanbutton(){
     document.getElementById('horrortabelle').style.visibility="hidden";
     document.getElementById('romantabelle').style.visibility="visible";
+
 }
 
+/*
+function onoff () {
 
+    if (document.getElementById("romantabelle").style.display = "none")
+    {
+            document.getElementById("horrortabelle").style.display = "inline";
+        }
+
+
+    elseif (document.getElementById("romantabelle").style.display = "inline")
+    {
+        document.getElementById("horrortabelle").style.display = "none";
+    }
+}
+*/
+
+/* Erstellt Tabelle*/
 function erstelleTabelle(roman) {
 
-
+    /* Erstellt Spalten*/
     document.writeln("<table border='1'><tr>");
 
     document.writeln("<th>Autor</th>");
@@ -40,7 +63,7 @@ function erstelleTabelle(roman) {
     document.writeln("<th>Erscheinungsjahr</th>");
     document.writeln("<th>Auflage</th></tr>");
 
-
+    /* Fügt werte über for-schelife ein*/
     for (var i = 0; i < roman.length; i++) {
 
         document.writeln("<tr><td>" + roman[i].autor + "</td>");
@@ -55,9 +78,10 @@ function erstelleTabelle(roman) {
     document.writeln("</table>");
 }
 
+/* Erstellt Tabelle*/
 function erstelleTabelle(horror) {
 
-
+    /* Erstellt Spalten*/
     document.writeln("<table border='1'><tr>");
 
     document.writeln("<th>Autor</th>");
@@ -68,7 +92,7 @@ function erstelleTabelle(horror) {
     document.writeln("<th>Erscheinungsjahr</th>");
     document.writeln("<th>Auflage</th></tr>");
 
-
+    /* Fügt werte über for-schelife ein*/
     for (var i = 0; i < horror.length; i++) {
 
         document.writeln("<tr><td>" + horror[i].autor + "</td>");
