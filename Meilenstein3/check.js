89fc3d3bf92c08aeb7fa4a2ea61d896205647a47
Philipp <scholz_philipp@gmx.de>
2015-05-18 07:00:27 +0200

    last changes!

---
+++ Meilenstein3/check.js
@@ -0,0 +1,100 @@
+
+function checkjava() {
+
+    var bool = false;
+    var fehler = true;
+
+    function exception() {
+        if (fehler == false) {
+            alert(unescape("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben"));
+        }
+        return fehler;
+    }
+
+    var btitel = document.getElementById("btitel").value;
+
+    if (btitel.length < 1) {
+        if (bool == false) {
+            document.getElementById("btitel").focus();
+            bool = true;
+        }
+        document.getElementById("btitel").style.borderColor = "red";
+        exception();
+    }
+
+    var autor = document.getElementById("autor").value;
+    if (!(autor.match(/[a-zA-Z]+$/))) {
+        if (bool == false) {
+            document.getElementById("autor").focus();
+            bool = true;
+        }
+        document.getElementById("autor").style.borderColor = "red";
+        exception();
+    }
+
+    var isbn = document.getElementById("isbn").value;
+
+    if (isbn.match([0-9]+$)) {
+
+        if (isbn.length != 13) {
+            if (bool == false) {
+                document.getElementById("isbn").focus();
+                bool = true;
+            }
+            document.getElementById("isbn").style.borderColor = "red";
+            exception();
+        }
+    }
+
+    var jahr = document.getElementById("jahr").value;
+
+    if (jahr.match([0-9]+$)) {
+
+        if (jahr.length != 4) {
+
+            document.getElementById("jahr").style.borderColor = "red";
+            exception();
+        }
+        else {
+        if (bool == false) {
+            document.getElementById("jahr").focus();
+            bool = true;
+        }
+        document.getElementById("jahr").style.borderColor = "red";
+            exception();
+    }
+
+
+    var auflage = document.getElementById("auflage").value;
+    if (auflage.match([0-9]+$)) {
+        if(bool = false){
+            document.getElementById("auflage").focus();
+            bool = true;
+        }
+    }
+        document.getElementById("auflage").style.borderColor = "red";
+        exception();
+    }
+
+
+    var vorname = document.send.vorname.value;
+    if (!(vorname.match(/[a-zA-Z]+$/))) {
+        if (bool == false) {
+            document.send.vorname.focus();
+            bool = true;
+        }
+        document.send.vorname.style.borderColor = "red";
+        exception();
+    }
+
+    var name = document.getElementById("nachname").value;
+    if (!(name.match(/[a-zA-Z]+$/))) {
+        if (bool == false) {
+            document.getElementById("vorname").focus();
+            bool = true;
+        }
+        document.getElementById("vorname").style.borderColor = "red";
+        exception();
+    }
+
+}
