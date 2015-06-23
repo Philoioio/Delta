ea878e1fe86d6f149a5b7d1329a2b3e4367dd813
Minzl <m.ostermaier@googlemail.com>
2015-06-24 00:49:10 +0200

    Komplett überarbeitet

--- Meilenstein5/getBooks.php
+++ Meilenstein5/getBooks.php
@@ -1,30 +1,20 @@
+
 <?php
 
 header('Content-Type: application/json');
 
-$horror_books_file = "../json/horror_books.json";
-$roman_books_file = "../json/roman_books.json";
 
-// get category from GET-request
-$data = $_GET["json"];
-
-if (loadDatabase(horror) || $data == horror) {
-    $data = file_get_contents($horror_books_file);
+if ($_GET['json'] == 'roman') {
+    $filename = '../Meilenstein3/roman_books.json';
+} elseif($_GET['json'] == 'horror') {
+    $filename = '../Meilenstein3/horror_books.json';
 }
 
-if (loadDatabase(roman) || $data == horror) {
-    $data = file_get_contents($roman_books_file);
-}
 
-if ($data != null && $data != false) {
-    header('Content-Type: application/json');
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-    header('Access-Control-Allow-Credentials: true');
-    echo $data;
-}
 
+if ($_GET['json'] != null && $_GET['json'] != false) {
+    echo file_get_contents($filename);
+}
 
 ?>
 
