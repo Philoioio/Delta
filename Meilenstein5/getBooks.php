71805ae3eebf550306a4eedfa30dc87466ff6369
Minzl <m.ostermaier@googlemail.com>
2015-06-24 12:40:32 +0200

    Update getBooks.php

--- Meilenstein5/getBooks.php
+++ Meilenstein5/getBooks.php
@@ -3,7 +3,7 @@
 
 header('Content-Type: application/json');
 
-
+//Wählt die jeweilge Json datei aus und speichert sie in der Variable $filename
 if ($_GET['json'] == 'roman') {
     $filename = '../Meilenstein3/roman_books.json';
 } elseif($_GET['json'] == 'horror') {
@@ -11,7 +11,7 @@ if ($_GET['json'] == 'roman') {
 }
 
 
-
+//Gebe daten mit Echo aus
 if ($_GET['json'] != null && $_GET['json'] != false) {
     echo file_get_contents($filename);
 }
